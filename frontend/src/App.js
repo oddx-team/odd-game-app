a214a5b6752beacaa15906f447a274c538e726f4
mocmeo <vdev.lamnn@gmail.com>
2020-03-29 21:20:47 +0800

    UPD: FE - Add panel signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import PanelLogin from './components/panels/PanelLogin';
+import PanelRegister from './components/panels/PanelRegister';
 
 function App() {
   return (
     <div id='app'>
       <div className='main'>
-        <PanelLogin />
+        {/* <PanelLogin /> */}
+        <PanelRegister />
       </div>
     </div>
   );
