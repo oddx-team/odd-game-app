54531847a6cbec490292decfe8186cf55c9d0674
Au Le <lethehaiau@gmail.com>
2020-04-20 23:04:02 +0700

    Global chat intitialize

---
+++ frontend/src/components/GlobalChat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GlobalChat.scss';
+
+const GlobalChat = () => {
+  return (
+    <div className='global-chat'>
+      
+    </div>
+  )
+}
+
+export default GlobalChat;
