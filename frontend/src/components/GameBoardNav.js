d8cb7b086104b7ad2f3ac588d7df5d09250d0651
mocmeo <vdev.lamnn@gmail.com>
2020-04-21 20:34:44 +0800

    UPD: FE - Refactor game board nav

--- frontend/src/components/GameBoardNav.js
+++ frontend/src/components/GameBoardNav.js
@@ -8,26 +8,21 @@ import './GameBoardNav.scss';
 const GameBoardNav = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const active = (idx) => {
+    return idx === activeIndex ? 'active' : '';
+  };
+
   return (
     <div className='game-board-nav'>
-      <div
-        className={classNames('tab-new', { active: activeIndex === 0 })}
-        onClick={(e) => setActiveIndex(0)}
-      >
+      <div className={classNames('tab-new', active(0))} onClick={(e) => setActiveIndex(0)}>
         <img src={IconNew} />
         <span>New</span>
       </div>
-      <div
-        className={classNames('tab-vn', { active: activeIndex === 1 })}
-        onClick={(e) => setActiveIndex(1)}
-      >
+      <div className={classNames('tab-vn', active(1))} onClick={(e) => setActiveIndex(1)}>
         <img src={IconVN} />
         <span>VN games</span>
       </div>
-      <div
-        className={classNames('tab-global', { active: activeIndex === 2 })}
-        onClick={(e) => setActiveIndex(2)}
-      >
+      <div className={classNames('tab-global', active(2))} onClick={(e) => setActiveIndex(2)}>
         <img src={IconGlobal} />
         <span>Global games</span>
       </div>
