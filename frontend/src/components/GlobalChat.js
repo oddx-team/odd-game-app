704acce4206ffcf9e27cc4bf08f0de00fef89d1e
Au Le <lethehaiau@gmail.com>
2020-04-21 20:08:40 +0700

    Add global chat

--- frontend/src/components/GlobalChat.js
+++ frontend/src/components/GlobalChat.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import TextInput from './basics/TextInput';
 import './GlobalChat.scss';
 
 const GlobalChat = () => {
+  const onMessageSubmit = (text) => {
+    //handle new message here
+  };
+
   return (
     <div className='global-chat'>
-      
+      <div className='global-chat--banner'>
+        <span>Chat room</span>
+      </div>
+      <div className='global-chat--content'></div>
+      <div className='global-chat--input'>
+        <TextInput placeholder='Type here' onSubmit={onMessageSubmit} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default GlobalChat;
