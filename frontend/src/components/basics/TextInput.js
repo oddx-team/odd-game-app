8755fd356e5f0c1ae10873de51506047d31a5963
Au Le <lethehaiau@gmail.com>
2020-04-21 19:53:50 +0700

    add TextInput component //new folder to store basic component that ready to use

---
+++ frontend/src/components/basics/TextInput.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const TextInput = (props) => {
+  const [value, setValue] = useState();
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    if (props.onSubmit) props.onSubmit(value);
+    setValue('');
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={value}
+        onChange={handleChange}
+        style={{ fontSize: '100%' }}
+        placeholder={props.placeholder}
+      />
+      <input type='submit' value='Submit' style={{ display: 'none' }} />
+    </form>
+  );
+};
+
+TextInput.propTypes = {
+  placeholder: PropTypes.string,
+  onSubmit: PropTypes.func,
+};
+
+export default TextInput;
