47f5f3308b6127a383d1ffd763a11fcc97aa5b23
mocmeo <vdev.lamnn@gmail.com>
2020-04-22 20:05:20 +0800

    UPD: FE - Customize game cards

--- frontend/src/components/GameBoard.js
+++ frontend/src/components/GameBoard.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import OddCard from './oddx/OddCard.js';
 
+import './GameBoard.scss';
+
 const GameBoard = () => {
   return (
     <div className='game-board'>
-      <OddCard />
+      <OddCard className='black' />
+      <OddCard className='white' />
+      <OddCard className='white medium' />
+      <OddCard className='black medium' />
     </div>
   );
 };
--- frontend/src/components/oddx/OddCard.js
+++ frontend/src/components/oddx/OddCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import IconLogo from 'cdn/assets/logo.png';
+import classNames from 'classnames';
 import { sizeWH, textMixin, flexCenter, position } from 'mixins';
 
 const OddCard = (props) => {
+  const cardClass = classNames('block', props.className);
+
   return (
-    <StyledCard>
+    <StyledCard className={cardClass}>
       <div id='title'>Donald Trump has nominated ___ for his VP!</div>
       <div id='logo'>
         <img src={IconLogo} />
@@ -22,12 +25,11 @@ const OddCard = (props) => {
 const StyledCard = styled.div`
   @import 'styles/global.scss';
 
-  @include sizeWH(3rem, 2.8rem);
   ${sizeWH('3rem', '2.8rem')}
   background: #212121;
   border-radius: 0.05rem;
   padding: 0.2rem;
-  margin: 0.3rem;
+  margin: 0.3rem 0.2rem 0 0;
   position: relative;
 
   & > #title {
@@ -62,7 +64,63 @@ const StyledCard = styled.div`
       ${sizeWH('0.27rem', '0.27rem')}
       background: #fff;
       border-radius: 50%;
-      padding-top: 0.04rem;
+      padding-top: 0.02rem;
+    }
+  }
+
+  &.white {
+    background: #fff;
+
+    & > #title {
+      ${textMixin({ color: '#000', size: '0.25rem', align: 'left' })}
+    }
+
+    #logo {
+      span {
+        ${textMixin({ color: '#000', size: '0.22rem' })}
+      }
+    }
+
+    #picker {
+      & > div:first-child {
+        ${textMixin({ color: '#000', size: '0.23rem', weight: 'bold', transform: 'uppercase' })}
+      }
+
+      & > div:last-child {
+        ${textMixin({ color: '#fff', size: '0.2rem' })}
+        background: #000;
+      }
+    }
+  }
+
+  &.medium {
+    ${sizeWH('2.2rem', '2rem')}
+
+    & > #title {
+      font-size: 0.2rem;
+    }
+
+    #logo {
+      img {
+        ${sizeWH('0.22rem', '0.22rem')}
+      }
+      span {
+        font-size: 0.18rem;
+      }
+    }
+
+    #picker {
+      ${position({ pos: 'absolute', bottom: '0.2rem', left: '1.3rem' })}
+
+      & > div:first-child {
+        font-size: 0.19rem;
+        margin-right: 0.06rem;
+      }
+
+      & > div:last-child {
+        ${sizeWH('0.23rem', '0.23rem')}
+        font-size: 0.17rem;
+      }
     }
   }
 `;
