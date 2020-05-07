b75e86daf5807ef0f991ec661fbe17a58bd3e0a9
mocmeo <vdev.lamnn@gmail.com>
2020-05-07 23:03:37 +0800

    UPD: Minor fix for chat room

--- src/stylesheets/panels/PanelChat.style.js
+++ src/stylesheets/panels/PanelChat.style.js
@@ -16,9 +16,10 @@ export const StyledInput = styled.div`
   display: flex;
   color: #000;
   font-size: 0.18rem;
-  width: 4.5rem;
+  width: 3.5rem;
   height: 0.4rem;
   border-radius: 0.3rem;
-  background: rgba($color: #000, $alpha: 0.05);
+  background: #eee;
   margin: 0.1rem;
+  padding-top: 0.08rem;
 `;
