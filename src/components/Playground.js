e6923aa6cbaf6dfd813407820d5df6b7f564829f
mocmeo <vdev.lamnn@gmail.com>
2020-05-07 18:01:38 +0800

    UPD: FE - Add chat room template

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OddCard from './oddx/OddCard.js';
 
 import PlaygroundScore from './PlaygroundScore';
+import PlaygroundChat from './PlaygroundChat';
 import 'stylesheets/Playground.scss';
 
 const Playground = () => {
@@ -9,6 +10,7 @@ const Playground = () => {
     <div className="playground">
       <OddCard black />
       <PlaygroundScore />
+      <PlaygroundChat />
     </div>
   );
 };
---
+++ src/components/PlaygroundChat.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import CssModules from 'react-css-modules';
+import OddChatMessage from 'components/oddx/OddChatMessage';
+import OddTextInput from 'components/oddx/OddTextInput';
+import styles from 'stylesheets/PlaygroundChat.module.scss';
+
+const PlaygroundChat = () => {
+  return (
+    <div styleName="playground-chat">
+      <div styleName="header">Chat room</div>
+      <div styleName="container">
+        <div styleName="content">
+          <OddChatMessage user="admin" message="Hello world!" time={Date.now()} />
+          <OddChatMessage user="mocmeo" message="Hello world!" time={Date.now()} />
+          <OddChatMessage user="bigdaddy" message="Hello world!" time={Date.now()} />
+          <OddChatMessage user="mocmeo" message="Hello world!" time={Date.now()} />
+        </div>
+
+        <div styleName="input">
+          <OddTextInput placeholder="Type a message" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CssModules(PlaygroundChat, styles);
