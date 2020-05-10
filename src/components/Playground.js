91d9957addf9b10913d98f399b62ba00d2594133
mocmeo <vdev.lamnn@gmail.com>
2020-05-10 21:00:48 +0800

    UPD: FE - Playground cards

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -6,9 +6,35 @@ import PlaygroundCollection from './PlaygroundCollection';
 import 'stylesheets/Playground.scss';
 
 const Playground = () => {
+  const oddCards = Array(4)
+    .fill(null)
+    .map((_, i) => ({
+      text: 'Donald Trump has nominated __ for his VP',
+      color: 'white',
+    }));
+
   return (
     <div className="playground">
-      <OddCard black />
+      <div className="message">Select a card to play!</div>
+      <div className="container">
+        <div className="card-black">
+          <div className="title">*Black card:</div>
+          <OddCard black />
+          <button className="btn-select block gray">Confirm</button>
+        </div>
+
+        <div className="card-white">
+          <div className="title">The white cards played this round:</div>
+          <div className="cards">
+            {oddCards.map((_, i) => (
+              <div key={i}>
+                <OddCard white medium={oddCards.length <= 4} small={oddCards.length > 4} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
       <PlaygroundWidgets />
       <PlaygroundCollection />
     </div>
