80a0e8337975206ba1e96fbf645db2f660b175f4
Lam Ngo <ngonl@garena.com>
2020-08-29 23:55:11 +0800

    UPD: Clean up

--- src/pages/playground/Playground.js
+++ src/pages/playground/Playground.js
@@ -66,11 +66,6 @@ export const PagePlayground = () => {
         }
       })
     })()
-
-    // return () => {
-    //   window.socket.disconnect()
-    //   window.socket.close()
-    // }
   }, [setPlaygroundData, slug, socket])
 
   const confirmSelection = () => {
