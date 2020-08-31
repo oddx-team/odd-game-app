a093035fffd41ae30fdd95c7f59fc22415da0ba5
Lâm Ngô <ngonl@garena.com>
2020-08-31 12:09:35 +0800

    Add customizable button + icon templates (#45)
    * UPD: Restructure project
    
    * UPD: Continue structuring
    
    * UPD: Add some js mixins
    
    * UPD: Add button & icon to library
    
    * UPD: Test button
    
    * UPD: Change button blue color
    
    * UPD: Test with icon
    
    * UPD: Minor
    
    * UPD: Update font name
    
    * UPD: Update font weight

--- src/shared/utils/styles.js
+++ src/shared/utils/styles.js
@@ -49,14 +49,6 @@ export const mixin = {
     Color(colorValue)
       .alpha(opacity)
       .string(),
-  getImage: (path, width, height) => {
-    const cdn = '../../assets'
-    return `
-      width: ${width}rem;
-      height: ${height}rem;
-      background: url(${require(`${cdn}/${path}`)}) center / contain no-repeat;
-    `
-  },
   flexCenter: css`
     display: inline-flex;
     align-items: center;
