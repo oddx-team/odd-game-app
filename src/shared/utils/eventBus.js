9c15f74884b24b1b7d02b9259bbf3e9a29bb4f42
Tu Huynh <tu.huynh@linecorp.com>
2020-09-15 16:50:58 +0700

    Add @oddx/pubsub

--- src/shared/utils/eventBus.js
+++ src/shared/utils/eventBus.js
@@ -1,14 +1,3 @@
-export const eventBus = {
-  events: {},
-  publish (event, data) {
-    if (!this.events[event]) return
-    this.events[event].forEach(callback => callback(data))
-  },
-  subscribe (event, callback) {
-    if (!this.events[event]) this.events[event] = []
-    this.events[event].push(callback)
-  },
-  unsubscribe: event => {
-    delete this.events[event]
-  }
-}
+import usePubSub from '@oddx/pubsub'
+
+export const eventBus = usePubSub()
