9b110ad07501f8b3275a2079cd6b6bcb93981b38
Lam Ngo <ngonl@garena.com>
2020-11-02 21:51:15 +0800

    UPD: Minor

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -8,7 +8,7 @@ export const PageNotFound = ({ history }) => {
 
   return (
     <div className='page-notfound'>
-      <div className='panel-decor' />
+      <div className='panel-space' />
       <div className='panel-lost'>
         <h1>Are you lost?</h1>
         <p>You are accessing a page that does not exist or has been deleted / replaced. Don't worry, you can still go back to the homepage.</p>
